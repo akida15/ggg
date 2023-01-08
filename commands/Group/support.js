577f6c4ae0a24d6a0a2b4631e8654da5d28cd38b
sirajarj17 <96668437+sirajarj17@users.noreply.github.com>
2023-01-08 01:26:10 +0100

    Add files via upload

---
+++ commands/Group/support.js
@@ -0,0 +1,16 @@
+const { tlang } = require('../../lib/scraper')
+const fs = require('fs')
+
+module.exports = {
+    name: 'support',
+    category: 'weeb',
+    desc: 'Sends SECKTOR userbot group link.',
+    async exec(citel, Void,args) {
+        citel.reply(`*Check your Pm ${tlang().greet}*`);
+        await Void.sendMessage(`${citel.sender}`, {
+          image: fs.readFileSync("../../lib/assets/SocialLogo 1.png"),
+          caption: `*Group Name: Secktor-Support*\n*Group Link:* https://chat.whatsapp.com/Bl2F9UTVU4CBfZU6eVnrbC`,
+        });
+ 
+    }
+ }
